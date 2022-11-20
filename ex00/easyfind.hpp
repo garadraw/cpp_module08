843d7e1be876685f8be33d652d0a57dab7e6d160
Nartex <garadraw@gmail.com>
2022-11-20 21:43:47 +0100

    Added ex00 for Testing on MAC

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: vsimeono <vsimeono@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 12:09:37 by vsimeono          #+#    #+#             */
-/*   Updated: 2022/11/20 12:10:32 by vsimeono         ###   ########.fr       */
+/*   Updated: 2022/11/20 21:23:46 by vsimeono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,15 @@
 #define EASYFIND_HPP
 
 #include <iostream>
+#include <vector>
 
+template <typename T>
+int	easyfind(std::vector<T>list, int needle)
+{
+	for (typename std::vector<T>::iterator it = list.begin(); it!= list.end(); ++it)
+	{
+		if (*it == needle)
+			return (*it);
+	}
+	return (-1);
+}
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vsimeono <vsimeono@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 12:09:45 by vsimeono          #+#    #+#             */
-/*   Updated: 2022/11/20 12:11:13 by vsimeono         ###   ########.fr       */
+/*   Updated: 2022/11/20 21:43:04 by vsimeono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int main()
 {
-	
+
+	int arr[] = {16, 2, 77, 29};
+	std::vector<int> lst(arr, arr + sizeof(arr) / sizeof(int)); 
+	int rs = ::easyfind(lst, 77);
+	std::cout << rs << std::endl;
+
+	return (0);
 }
